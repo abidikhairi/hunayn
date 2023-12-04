7a322b152f3fbaa0d864c623d9cf24f9aa2372fd
abidikhairi <abidikhairi.1337@gmail.com>
2023-12-04 23:17:58 +0100

    add auto import for HunaynTrainer

--- hunayn/model/__init__.py
+++ hunayn/model/__init__.py
@@ -3,4 +3,4 @@ from hunayn.model.embedding import Embedding
 from hunayn.model.positional_encoding import PositionalEncoding
 from hunayn.model.encoder import EncoderLayer, TransformerEncoder
 from hunayn.model.decoder import DecoderLayer, TransformerDecoder
-from hunayn.model.transformer import Hunayn
+from hunayn.model.transformer import Hunayn, HunaynTrainer
