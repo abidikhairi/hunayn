aed5c39a7d8af3b55d4166087fdac99d975469da
abidikhairi <abidikhairi.1337@gmail.com>
2023-12-10 03:04:20 +0100

    add pytorch lightning helpers

--- hunayn/utils/__init__.py
+++ hunayn/utils/__init__.py
@@ -3,3 +3,4 @@ from hunayn.utils.cli_parser import get_default_parser
 from hunayn.utils.logging import get_default_logger
 from hunayn.utils.cloning import clones
 from hunayn.utils.optimizer import scheduler_fn
+from hunayn.utils.lightning import lightning_loggers, lightning_callbacks
---
+++ hunayn/utils/lightning.py
@@ -0,0 +1,53 @@
+"""Lightning utils"""
+import os
+from pytorch_lightning import loggers as pl_loggers, callbacks as pl_callbacks
+
+
+def lightning_loggers():
+    """
+    Initialize and return a list of Lightning loggers.
+
+    Returns:
+        List[pl_loggers.BaseLogger]: A list of Lightning loggers, including CSVLogger and WandbLogger if enabled.
+
+    Notes:
+        - CSVLogger logs training and validation metrics to a CSV file.
+        - WandbLogger logs metrics to the Weights & Biases service if the 'USE_WANDB' environment variable is set.
+
+    Example:
+        ```python
+        # Example usage:
+        # loggers = lightning_loggers()
+        # trainer = pl.Trainer(logger=loggers)
+        # ```
+    """
+    loggers = []
+    loggers.append(pl_loggers.CSVLogger('logs', name="hunayn", version=1))
+
+    if os.environ["USE_WANDB"] is not None:
+        loggers.append(pl_loggers.WandbLogger(project="hunayn", experiment="toasty-dragon-11"))
+
+    return loggers
+
+def lightning_callbacks():
+    """
+    Initialize and return a list of Lightning callbacks.
+
+    Returns:
+        List[pl_callbacks.Callback]: A list of Lightning callbacks, including ModelCheckpoint.
+
+    Notes:
+        - ModelCheckpoint saves the model's checkpoints during training based on specified conditions.
+
+    Example:
+        ```python
+        # Example usage:
+        # callbacks = lightning_callbacks()
+        # trainer = pl.Trainer(callbacks=callbacks)
+        # ```
+    """
+    callbacks = []
+    callbacks.append(pl_callbacks.ModelCheckpoint(
+        dirpath="models/checkpoints", filename="hunayn_v1", monitor="valid/loss"))
+
+    return callbacks
