61febd751f5b7525d370ef521a8f5777ec677ed6
abidikhairi <abidikhairi.1337@gmail.com>
2023-12-17 20:31:57 +0100

    set checkpoint path as argument to test.py

--- test.py
+++ test.py
@@ -1,21 +1,26 @@
 """Main file for training Hunayn"""
-from pytorch_lightning import Trainer
+import argparse
+from pytorch_lightning import Trainer, loggers
 from transformers import PreTrainedTokenizerFast
 
 from hunayn.dataset import get_test_dataloader
 from hunayn.model import HunaynTrainer
 
 
-def main():
+def main(args):
     """
     Execute the main testing routine.
 
-    This function loads the necessary tokenizers and the pre-trained model, creates a test data loader,
-    and runs the testing using a Lightning Trainer.
+    This function loads the necessary tokenizers, the pre-trained model from a specified checkpoint path,
+    creates a test data loader, and runs the testing using a Lightning Trainer.
 
     Args:
-        None
-
+        args (Namespace): Command-line arguments containing the path to the checkpoint.
+    Example:
+        ```python
+        # Example command-line usage:
+        # python test.py --checkpoint-path models/hunayn/checkpoints/hunayn_v1.ckpt
+        ```
     Returns:
         None
     """
@@ -24,18 +29,23 @@ def main():
     tgt_tknzr = PreTrainedTokenizerFast.from_pretrained(
         "models/hunayn/english_tokenizer")
 
-    model = HunaynTrainer.load_from_checkpoint(
-        'models/hunayn/checkpoints/hunayn_v1.ckpt')
+    model = HunaynTrainer.load_from_checkpoint(args.checkpoint_path)
 
     nheads = model.model_config.nhead
 
     test_loader = get_test_dataloader(
         'data/testset.tsv', src_tknzr, tgt_tknzr, batch_size=2, num_workers=4, nheads=nheads)
 
-    trainer = Trainer(accelerator='gpu')
+    csv_logger = loggers.CSVLogger(save_dir="logs", name="tests", version=1)
+
+    trainer = Trainer(accelerator='gpu', logger=[csv_logger])
 
     trainer.test(model, test_loader)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--checkpoint-path', required=True, type=str, help="Model checkpoint path")
+
+    main(parser.parse_args())
